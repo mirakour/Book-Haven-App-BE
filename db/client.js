1ed998199859101a6545f52fb4768100212b8f6c
kourtney_miranda <mirandakourt@gmail.com>
2025-06-26 14:20:03 -0700

    Fix Render Postgres SSL connection

--- db/client.js
+++ db/client.js
@@ -2,6 +2,11 @@ import { Client } from "pg";
 import dotenv from "dotenv";
 dotenv.config();
 
-const db = new Client({ connectionString: process.env.DATABASE_URL });
+const db = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // 🔒 Required for Render-hosted Postgres
+  },
+});
 
 export default db;
